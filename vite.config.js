1056ee1c6c5fdb7eccdc1b464fdb1a788dede395
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2025-12-25 13:06:25 +0530

    Netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/github_wrapper/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      }
-    }
-  }
 })
